c4e9d044021482e86e62a924dc61fd2f081ccd48
Marina Tishchenko <Ystalaaa@yandex.ru>
2023-11-06 02:18:02 +0300

    adds project

---
+++ src/main/java/UserData.java
@@ -0,0 +1,20 @@
+public class UserData {
+    public static User defaultUserData() {
+        return new User("ExampleTestUsr", "ExampleTestUsr@test.ru", "1234Qwer");
+    }
+    public static User defaultUserLogin() {
+        return new User("ExampleTestUsr@test.ru","1234Qwer");
+    }
+    public static User userDataIncorrectPassword() {
+        return new User("ExampleTestUsr", "ExampleTestUsr@test.ru", "123");
+    }
+    public static User userDataNullName() {
+        return new User(null, "ExampleTestUsr@test.ru", "1234Qwer");
+    }
+    public static User userDataNullEmail() {
+        return new User("ExampleTestUsr", null, "1234Qwer");
+    }
+    public static User userDataNullPassword() {
+        return new User("ExampleTestUsr", "ExampleTestUsr@test.ru", null);
+    }
+}
